e0fa4da6f39f7b63916d476934e96fbe5036979d
Alu1DAW16 <Alu1DAW16@ifc2.piobaroja.es>
2024-01-25 12:35:42 +0100

    holaktal

--- concesionario_java/src/util/coche.java
+++ concesionario_java/src/util/coche.java
@@ -1,5 +1,25 @@
 package util;
 
+import java.time.LocalDate;
+
 public class coche {
+private String matricula;
+private int Numero_puertas;
+private String tipo_combustible;
+private String marca;
+private String modelo;
+private int numero_facturacion;
+private LocalDate ultima_DGT;
+
+public coche(String matricula, int Numero_puertas, String tipo_combustible, String marca, String modelo, int numero_facturacion, int año, int mes, int dia ) {
+	this.matricula = matricula;
+	this.Numero_puertas = Numero_puertas;
+	this.tipo_combustible = tipo_combustible;
+	this.marca = marca;
+	this.modelo = modelo;
+	this.numero_facturacion = numero_facturacion;
+	this.ultima_DGT = LocalDate.of(año, mes, dia);
+	
+}
 
 }
