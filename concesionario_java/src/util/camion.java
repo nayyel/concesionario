73e6908d904f12a219d1fcdb822c3c8335fe7612
AmineVS <aminbouali13@gmail.com>
2024-01-29 12:17:57 +0100

    camion

---
+++ concesionario_java/src/util/camion.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.time.LocalDate;
+
+public class camion {
+
+	private String matricula;
+	private int Numero_plazas;
+	private String tipo_combustible;
+	private String marca;
+	private String modelo;
+	private int numero_facturacion;
+	private LocalDate ultima_DGT;
+
+	public camion (String matricula, int Numero_plazas, String tipo_combustible, String marca, String modelo, int numero_facturacion, int año, int mes, int dia ) {
+		this.matricula = matricula;
+		this.Numero_plazas = Numero_plazas;
+		this.tipo_combustible = tipo_combustible;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.numero_facturacion = numero_facturacion;
+		this.ultima_DGT = LocalDate.of(año, mes, dia);
+		
+	}
+}
